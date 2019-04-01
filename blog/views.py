c2ded1e922c94add5a3f726aac52a925e6f536fa
afraca130 <zokatn.gmail.com>
2019-04-01 16:57:06 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now())
+    qs = qs.order_by('published_date')
+
+    return render(request, 'blog/post_list.html', {
+        'post_list': qs,
+    })
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk = pk)
+    # pk = "100"
+    return render(request, 'blog/post_detail.html', {'post' : post, }) 
